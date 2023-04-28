eba572521d27750c3b9b7a242ef8ac69634a2983
Corseing <corseing.desarrollo@gmail.com>
2023-04-28 15:15:47 -0500

    Second Home commit

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
 import { View, Text } from "react-native";
 import React from "react";
 import Login from './screen/Login'
+import Home from './screen/Home'
 
 const App = () => {
     return(
-        <Login/>
+        // <Login/>
+        <Home/>
     )
 }
 export default App
