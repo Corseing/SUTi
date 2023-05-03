1aea028c2937573c0a9db1060fddbc1065eb8916
AndresB <corseing.desarrollo@gmail.com>
2023-05-02 22:59:12 -0500

    SUTi App 2nd commit

--- App.tsx
+++ App.tsx
@@ -1,10 +1,22 @@
-import { View, Text } from "react-native";
-import React from "react";
-import Login from './screen/Login'
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import 'react-native-gesture-handler';
+import OnBoardScreen from './src/views/screens/OnBoardScreen';
+import HomeScreen from './src/views/screens/HomeScreen';
+import DetailsScreen from './src/views/screens/DetailsScreen';
+const Stack = createStackNavigator();
 
 const App = () => {
-    return(
-        <Login/>
-    )
-}
-export default App
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{headerShown: false}}>
+        <Stack.Screen name="OnBoardScreen" component={OnBoardScreen} />
+        <Stack.Screen name="HomeScreen" component={HomeScreen} />
+        <Stack.Screen name="DetailsScreen" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
